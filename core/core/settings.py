07af420590deb810578c82260623a11e0b4389f1
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-07-05 23:05:42 +0300

    Updated profiles settings urls

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'profiles',
+     'profiles.apps.ProfilesConfig',
       'rest_framework',
      'django_extensions',
 ]
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL ='/media/'
+MEDIA_ROOT='uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

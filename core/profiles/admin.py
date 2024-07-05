435621aaf3fb3fb8293c65e64e4078421e5a441a
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-07-05 23:06:39 +0300

    Register  profil

--- core/profiles/admin.py
+++ core/profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from profiles.models import Profil,ProfilSituation
 # Register your models here.
+
+
+admin.site.register(Profil)
+admin.site.register(ProfilSituation)
